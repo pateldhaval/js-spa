a8b2dd013e8f85a36385a78621f973eb7f3114b7
Dhaval Patel <pateldhaval3890@gmail.com>
2022-08-27 09:51:13 +0530

    utils file separated

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,44 +1,4 @@
-// [All routes]
-export const routes = [
-	{ path: '/not-found', view: () => console.log('404 loaded!') },
-	{ path: '/', view: () => console.log('Dashboard loaded!') },
-	{ path: '/posts', view: () => console.log('Posts loaded!') },
-	{ path: '/settings', view: () => console.log('Settings loaded!') },
-];
-
-// [Navigation function]
-export const navigateTo = (location) => {
-	// [To change browser url]
-	history.pushState(null, null, location);
-
-	router();
-};
-
-// [Router function]
-const router = async () => {
-	// [Find a match using url from routes]
-	const potentialMatches = routes.map((route) => {
-		const isMatch = location.pathname === route.path;
-		return {
-			route,
-			isMatch,
-		};
-	});
-
-	// [Get the route that matches with url]
-	const match = potentialMatches.find((item) => item.isMatch);
-	console.log(match);
-
-	// [If no route matches, default to 404]
-	if (!match) {
-		return {
-			route: routes[0],
-			isMatch: true,
-		};
-	}
-
-	match.route.view();
-};
+import { navigateTo, router } from './utils.js';
 
 // [Document ready]
 document.addEventListener('DOMContentLoaded', () => {
