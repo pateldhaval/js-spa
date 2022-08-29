367ff7ab1015711da2d9dfa02a829cf6558c016e
Dhaval Patel <pateldhaval3890@gmail.com>
2022-08-29 14:31:22 +0530

    Dark mode styles

--- frontend/static/views/Settings.js
+++ frontend/static/views/Settings.js
@@ -9,7 +9,7 @@ export default class extends Abstract {
 	async getHtml() {
 		return `
 			<h1 class='block text-2xl font-bold text-gray-800 sm:text-3xl dark:text-white'>Settings</h1>
-			<p class="mt-2 text-lg text-gray-800 dark:text-gray-400">
+			<p class="mt-4">
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id sequi eum est nam nesciunt officia, explicabo consectetur perspiciatis cumque in eos! Debitis tempora quisquam adipisci fuga placeat sunt illum deserunt ea. Fuga labore qui harum ipsum debitis quibusdam sint nihil velit voluptatem quo. Vitae maiores commodi accusamus ratione? Deserunt, numquam. Id sequi eum est nam nesciunt officia, explicabo consectetur perspiciatis cumque in eos! Debitis tempora quisquam adipisci fuga placeat sunt illum deserunt ea. Fuga labore qui harum ipsum debitis quibusdam sint nihil velit voluptatem quo. Vitae maiores commodi accusamus ratione? Deserunt, numquam.
 			</p>
     `;
